812c48ef2a64c109a56f04a4978438ff3f05be60
Moontean <alexandru.muntean1@isa.utm.md>
2024-09-11 16:40:00 +0300

    Navigation bar

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,9 +1,22 @@
-const DashboardPage = () => {
+import prismadb from "@/lib/prismadb"
+
+interface DashboardProps {
+    params: {storeId: string}
+};
+
+const DashboardPage: React.FC<DashboardProps> = async ({
+    params
+}) => {
+    const store = await prismadb.store.findFirst({
+        where: {
+            id:params.storeId
+    }
+    });
+
     return (
         <div>
-            Dashboard
+            Active Store: {store?.name}
         </div>
     );
 }
-
 export default DashboardPage;
--- ecommerce-admin/app/(dashboard)/[storeId]/layout.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/navbar";
 import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs/server";
 import {redirect} from "next/navigation"
@@ -27,9 +28,7 @@ if (!userId) {
 
     return (
         <>
-        <div>
-            This will be a Navbar
-        </div>
+        <Navbar />
         {children}
         </>
     )
